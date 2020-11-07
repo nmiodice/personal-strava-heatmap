67814f8fb551ba459ceed565bc32cb375b39341d
Nick Iodice <niiodice@microsoft.com>
2020-11-07 05:27:43 -0600

    Add go-to-location button to map. Add coordinates to URL.

--- api/static/map.js
+++ api/static/map.js
@@ -1,78 +1,135 @@
+
+function resetWindowParams() {
+  window.params = new URLSearchParams(window.location.search);
+}
+
 /**
  * Adds tile image overlay when pannign around the map
  */
 class CoordMapType {
   constructor(tileSize) {
-      this.tileSize = tileSize;
+    this.tileSize = tileSize;
   }
   getTile(coord, zoom, ownerDocument) {
-      const img = ownerDocument.createElement("img");
-      const endpoint = $('#tile_endpoint')[0].value
-      const map_id = $('#map_id')[0].value
-      const tile_name = '-' + coord.x + '-' + coord.y + '-' + zoom + '.png'
+    const img = ownerDocument.createElement("img");
+    const endpoint = $('#tile_endpoint')[0].value
+    const map_id = $('#map_id')[0].value
+    const tile_name = '-' + coord.x + '-' + coord.y + '-' + zoom + '.png'
 
-      img.onerror = "this.style.display='none';"
-      img.alt = ""
-      img.src = endpoint + map_id + tile_name
-      return img
+    img.onerror = "this.style.display='none';"
+    img.alt = ""
+    img.src = endpoint + map_id + tile_name
+    return img
   }
-  releaseTile(tile) {}
+  releaseTile(tile) { }
 }
 
 /**
 * Initialize map
 */
 function initMap() {
-  const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 13,
-      maxZoom: 19,
-      minZoom: 2,
-      center: { // Austin, TX
-          lat: 30.2729,
-          lng: -97.7444
-      },
-      mapTypeId: 'terrain',
-      mapTypeControlOptions: [],
-  });
-  map.setOptions({ styles: getMapStyle() });
+  resetWindowParams()
+  configureWindowMap()
+  configureMapListeners()
+  configureButtomListener()
+  applyMapOverlay()
+  triggerGPSEnablement()
+}
 
+function configureButtomListener() {
+  $('#location_button').click(function () {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function ({ coords: { latitude: lat, longitude: lng } }) {
+        map.setCenter({ lat, lng })
+        map.panTo({ lat, lng })
+      }, function () {
+        alert('You must grant access to your location in order to use this feature.')
+      });
+    }
+  });
+}
+
+function getQueryParam(paramName, defaultValue) {
+  val = window.params.get(paramName)
+  if (val != null) {
+    return (defaultValue.constructor)(val)
+  }
+
+  return defaultValue
+}
+
+function configureWindowMap() {
+  window.map = new google.maps.Map(document.getElementById("map"), {
+    zoom: getQueryParam('z', 13),
+    maxZoom: 19,
+    minZoom: 2,
+    center: { // Default is Austin, TX
+      lat: getQueryParam('lat', 30.2729),
+      lng: getQueryParam('lon', -97.7444)
+    },
+    mapTypeId: 'terrain',
+    mapTypeControlOptions: [],
+  });
+  window.map.setOptions({
+    styles: getMapStyle(),
+    zoomControl: true,
+    scaleControl: true,
+    mapTypeControl: false,
+    streetViewControl: false,
+    rotateControl: false,
+    fullscreenControl: false
+  });
+}
+
+function configureMapListeners() {
+  let positionListener = function () {
+    newParams = new URLSearchParams(window.params)
+    newParams.set('lat', window.map.getCenter().lat())
+    newParams.set('lon', window.map.getCenter().lng())
+    newParams.set('z', window.map.getZoom())
+
+    history.replaceState(null, null, "?" + newParams.toString());
+    resetWindowParams()
+  }
+
+  window.map.addListener("center_changed", positionListener);
+  window.map.addListener("zoom_changed", positionListener);
+}
+
+function applyMapOverlay() {
   // Insert this overlay map type as the first overlay map type at
   // position 0. Note that all overlay map types appear on top of
   // their parent base map.
-  map.overlayMapTypes.insertAt(
-      0,
-      new CoordMapType(new google.maps.Size(256, 256))
+  window.map.overlayMapTypes.insertAt(
+    0,
+    new CoordMapType(new google.maps.Size(256, 256))
   );
+}
 
-  // ask for user location after map loads
-  setTimeout(function(){ 
+function triggerGPSEnablement(map) {
+  setTimeout(function () {
     if (navigator.geolocation) {
-      navigator.geolocation.watchPosition(getPositionUpdateFunc(map), null, {});
+      navigator.geolocation.getCurrentPosition(setPositionMarker);
     }
   }, 0);
 }
 
-function getPositionUpdateFunc(map) {
-  return function({ coords: { latitude: lat, longitude: lng } }){
-    map.setCenter({lat, lng})
-    map.setZoom(15)
-    map.panTo({lat, lng})
-    if (typeof window.gpsMarker === 'undefined') {
-        window.gpsMarker = new google.maps.Marker({
-            position: {lat, lng},
-            map: map,
-            icon: {
-              path: google.maps.SymbolPath.CIRCLE,
-              scale: 8,
-              fillOpacity: 1,
-              strokeWeight: 2,
-              fillColor: '#5384ED',
-              strokeColor: '#ffffff',
-            },
-          });
-    } else {
-        window.gpsMarker.setPosition({lat, lng})
-    }
+function setPositionMarker({ coords: { latitude: lat, longitude: lng } }) {
+  if (typeof window.gpsMarker === 'undefined') {
+    window.gpsMarker = new google.maps.Marker({
+      position: { lat, lng },
+      map: map,
+      icon: {
+        path: google.maps.SymbolPath.CIRCLE,
+        scale: 8,
+        fillOpacity: 1,
+        strokeWeight: 2,
+        fillColor: '#5384ED',
+        strokeColor: '#ffffff',
+      },
+    });
+  } else {
+    window.gpsMarker.setPosition({ lat, lng })
   }
 }
 
