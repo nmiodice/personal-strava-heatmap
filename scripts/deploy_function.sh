cad02c6f5adc091387da27fcb5ac8bd0f09d8428
Nick Iodice <niiodice@microsoft.com>
2020-12-25 23:35:12 -0700

    Add language flag for function deployment script

--- scripts/deploy_function.sh
+++ scripts/deploy_function.sh
@@ -8,4 +8,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 verifyCommandExists "func"
 verifyEnvironmentVariableExists "AZURE_FUNCTION_NAME"
 
-(cd function && func azure functionapp publish "$AZURE_FUNCTION_NAME")
+(cd function && func azure functionapp publish "$AZURE_FUNCTION_NAME" --python)
