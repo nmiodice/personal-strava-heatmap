50561536dbdc9bc4cf1bf79b2e4ad8c0e7ddd46c
Nick Iodice <niiodice@microsoft.com>
2021-02-09 22:23:47 -0700

    Move from GRS storage to LRS storage

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -3,7 +3,7 @@ resource "azurerm_storage_account" "sa" {
   name                     = substr(replace(format("%s-storage", local.prefix), "-", ""), 0, 24)
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
-  account_replication_type = "GRS"
+  account_replication_type = "LRS"
   account_tier             = "Standard"
   allow_blob_public_access = true
   tags                     = local.tags
